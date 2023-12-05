7dbb45b4b8e416f41e6859b6676e78ef303958f3
geakiera11 <kieragea8@gmail.com>
2023-12-05 16:35:24 +0700

    tugas-akhir

---
+++ ecommerce/Pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hero w-full bg-gradient-to-r from-yellow-200 via-green-300 to-lightpink-400 min-h-screen flex flex-col">
+    <div class="hero-content flex-col lg:flex-row mt-10">
+      <div class="w-[600px] ml-[60px]">
+        <h1 class="text-5xl font-bold text-red-800">Selamat Datang di Toko G-Sport</h1>
+        <p class="mt-4 text-lg text-yellow-700">Toko kami menyediakan banyak produk untuk kalian yang ingin mencari perlengkapan untuk berolahraga!</p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ ecommerce/composables/constants/category.ts
@@ -0,0 +1,34 @@
+export const category: Category[] = [
+    {
+    id: 1,
+    name: "Baju",
+    },
+    {
+    id: 2,
+    name: "Sepatu",
+    },
+    {
+    id: 3,
+    name: "Celana",
+    },
+    {
+    id: 4,
+    name: "Jam",
+    },
+    {
+    id: 5,
+    name: "Bola",
+    },
+    {
+    id: 6,
+    name: "Tas Ransel",
+    },
+    {
+    id: 7,
+    name: "Kneepad",
+    },
+    ]
+    export interface Category {
+    id? : number,
+    name?: string,
+    }
